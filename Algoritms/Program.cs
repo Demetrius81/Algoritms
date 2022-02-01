19b76e547b0b7024bb872eba645c479807b43987
Dmitri <shizimaniak@gmail.com>
2022-02-01 10:02:31 +0300

    Создал отдельные классы для методов

--- Algoritms/Program.cs
+++ Algoritms/Program.cs
@@ -6,109 +6,11 @@ namespace Algoritms
 {
     class Program
     {
-        //  Задание 1
-        //  Требуется реализовать на C# функцию согласно блок-схеме. Блок-схема описывает алгоритм проверки, простое число или нет.
-        //⦁	Реализовать в виде консольного приложения.
-        //⦁	Алгоритм реализовать в отдельном классе согласно блок-схеме.
-        //⦁	Написать проверочный код(один положительный, один отрицательный сценарий) в отдельной функции и вызывать его при запуске.
-        //⦁	Код выложить на GitHub.
-
-        /// <summary>
-        /// Метод определяет простое число или нет. На вход подается переменная long. Результатом работы метода является переменная bool. 
-        /// </summary>
-        /// <param name="number">Исследуемое число</param>
-        /// <returns bool>Результат работы</returns>
-        static bool CheckPrimeOrNot(long number)
-        {
-            int d = 0;
-
-            for (int i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    d++;
-                }
-            }
-            if (d == 0)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Метод запрашивает у пользователя число и выводит в консоль является ли это число простым.
-        /// </summary>
-        static void WritePrimeOrNot()
-        {
-            long number = 0;
-
-            Console.Write($"Введите число >");
-
-            try
-            {
-                number = Convert.ToInt64(Console.ReadLine());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"{ex}");
-
-                return;
-            }
-
-            bool isPrime = CheckPrimeOrNot(number);
-
-            if (isPrime)
-            {
-                Console.WriteLine($"Число {number} является простым");
-            }
-            else
-            {
-                Console.WriteLine($"Число {number} не является простым");
-            }
-        }
-
-        /// <summary>
-        /// Тестовый метод
-        /// </summary>
-        static void WritePrimeOrNotTest()
-        {
-            long number = 7;
-
-            Console.WriteLine($"Введите число >{number}");
-
-            bool isPrime = CheckPrimeOrNot(number);
-
-            if (isPrime)
-            {
-                Console.WriteLine($"Число {number} является простым");
-            }
-            else
-            {
-                Console.WriteLine($"Число {number} не является простым");
-            }
-
-            number = 8;
-
-            Console.WriteLine($"Введите число >{number}");
-
-            isPrime = CheckPrimeOrNot(number);
-
-            if (isPrime)
-            {
-                Console.WriteLine($"Число {number} является простым");
-            }
-            else
-            {
-                Console.WriteLine($"Число {number} не является простым");
-            }
-        }
-
         static void Main(string[] args)
         {
-            WritePrimeOrNotTest();
+            PrimeNumber.PrimeNumber.WritePrimeOrNotTest();
 
-            WritePrimeOrNot();
+            PrimeNumber.PrimeNumber.WritePrimeOrNot();
 
             foreach (var item in Fibonacci.Fibonacci.FibonacciCalculateRecursive(Convert.ToInt32(Console.ReadLine())))
             {
