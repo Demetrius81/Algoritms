bb63a00a13024c8df0280799374875a0d0f63eb3
Dmitri <shizimaniak@gmail.com>
2022-02-01 10:48:44 +0300

    Создал класс и интерфейс

---
+++ DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    public class DoublyLinkedList : ILinkedList
+    {
+        /// <summary>
+        /// Возвращает количество элементов в списке
+        /// </summary>
+        /// <returns></returns>
+        public int GetCount()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Добавляет новый элемент списка
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddNode(int value)
+        {
+
+        }
+
+        /// <summary>
+        /// Добавляет новый элементсписка после определённого элемента
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="value"></param>
+        public void AddNodeAfter(DoublyLinkedList node, int value)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаляет элемент по порядковому номеру
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveNode(int index)
+        {
+
+        }
+
+        /// <summary>
+        /// Удаляет указанный элемент
+        /// </summary>
+        /// <param name="node"></param>
+        public void RemoveNode(DoublyLinkedList node)
+        {
+
+        }
+
+        /// <summary>
+        /// Ищет элемент по его значению
+        /// </summary>
+        /// <param name="searchValue"></param>
+        /// <returns></returns>
+        public DoublyLinkedList FindNode(int searchValue)
+        {
+            return null;
+        }
+    }
+}
