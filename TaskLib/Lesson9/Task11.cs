c15ae199326287664b9d5ec66e18b70a1c2081ca
Dmitri <Shizimaniak@gmail.com>
2022-07-07 11:13:11 +0300

    EazyStack<T> готов

--- TaskLib/Lesson9/Task11.cs
+++ TaskLib/Lesson9/Task11.cs
@@ -11,15 +11,31 @@ namespace Algoritms
 
         public override string TaskNumber { get => _taskNumber; }
 
-        private readonly string _taskName = $"Структуры данных. Стек.\t\t\t\t\t- задача 10";
+        private readonly string _taskName = $"Структуры данных. Стек.\t\t\t\t\t\t\t\t\t- задача 11";
 
         public override string TaskName { get => _taskName; }
 
         public override void TaskResultOutput()
         {
-            CountOptionsEightQueens countOptionsEightQueens = new CountOptionsEightQueens();
+            EazyStack<int> eazyStack = new EazyStack<int>();
 
-            countOptionsEightQueens.RunCheck();
+            eazyStack.Push(1);
+            eazyStack.Push(2);
+            eazyStack.Push(3);
+            eazyStack.Push(4);
+            eazyStack.Push(5);
+            eazyStack.Push(6);
+            eazyStack.Push(7);
+            eazyStack.Push(8);
+            eazyStack.Push(9);
+
+            var item = eazyStack.Pop();
+            var item2 = eazyStack.Peek();
+
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+
+            Console.ReadKey(true);
         }
 
     }
