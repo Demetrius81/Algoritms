33faa891983dc7887b7eda801c6c2850018370c9
Dmitri <Shizimaniak@gmail.com>
2022-07-07 10:35:13 +0300

    Начал работу над реализацией стека

---
+++ TaskLib/Lesson9/Task11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritms
+{
+    [Task]
+    class Task11 : BaseTask
+    {
+        private readonly string _taskNumber = "11";
+
+        public override string TaskNumber { get => _taskNumber; }
+
+        private readonly string _taskName = $"Структуры данных. Стек.\t\t\t\t\t- задача 10";
+
+        public override string TaskName { get => _taskName; }
+
+        public override void TaskResultOutput()
+        {
+            CountOptionsEightQueens countOptionsEightQueens = new CountOptionsEightQueens();
+
+            countOptionsEightQueens.RunCheck();
+        }
+
+    }
+}
