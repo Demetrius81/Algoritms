d52b26e780d87ea3a4d65c1c02bab248e507de53
Dmitri <Shizimaniak@gmail.com>
2022-07-12 10:16:03 +0300

    Подготовка классов

---
+++ TaskLib/Lesson12/Task13.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritms
+{
+    [Task]
+    class Task14 : BaseTask
+    {
+        private readonly string _taskNumber = "14";
+
+        public override string TaskNumber { get => _taskNumber; }
+
+        private readonly string _taskName = $"Структуры данных. Хеш-таблицы.\t\t\t\t\t\t\t\t- задача 14";
+
+        public override string TaskName { get => _taskName; }
+
+        public override void TaskResultOutput()
+        {
+            Console.Clear();
+
+            
+
+            Console.WriteLine();
+
+        }
+
+    }
+}
