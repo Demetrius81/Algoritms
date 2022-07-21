20ffb2d21f46b26c4c76773afe12cd226c9b2df8
Dmitri <Shizimaniak@gmail.com>
2022-07-21 10:42:18 +0300

    Двоичное дерево поиска алгоритм готов.

---
+++ TaskLib/Lesson15/Node.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Algoritms.Lesson15
+{
+    internal class Node
+    {
+        private int _value;
+        private Node _leftChild;
+        private Node _rightChild;
+
+        public int Value { get => _value; set => _value = value; }
+        internal Node LeftChild { get => _leftChild; set => _leftChild = value; }
+        internal Node RightChild { get => _rightChild; set => _rightChild = value; }
+    }
+}
