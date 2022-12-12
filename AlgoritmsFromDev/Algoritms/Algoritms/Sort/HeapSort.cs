6684208e0d47917aad6c533aba33a489e2ed5439
Dmitri <Shizimaniak@gmail.com>
2022-12-12 21:35:17 +0300

    Набросал алгоритм пирамидальной сортировки

---
+++ AlgoritmsFromDev/Algoritms/Algoritms/Sort/HeapSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms.Sort;
+internal class HeapSort
+{
+    public static void Sort(int[] array) =>
+        Sort(array, array.Length, array.Length / 2);
+
+    private static void Sort(int[] array, int length, int root)
+    {
+        int left = 2 * root + 1;
+        length = array.Length;
+        int right = 2 * root + 2;
+        int largest = root;
+
+        if (left < length && array[left] > array[largest])
+        {
+            largest = left;
+        }
+
+        if (right < length && array[right] > array[largest])
+        {
+            largest = right;
+        }
+
+        if (largest != root)
+        {            
+            (array[root], array[largest]) = (array[largest], array[root]);            
+            Sort(array, length, largest);
+        }
+    }
+}
